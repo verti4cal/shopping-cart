f3d8083b1f31430f32da103878a26d866bc969c5
Simon <shinze@adorum.de>
2024-06-17 16:28:55 +0200

    refactor: session storage

--- src/Storage/CartStorage.php
+++ src/Storage/CartStorage.php
@@ -5,6 +5,7 @@ namespace App\Storage;
 use App\Entity\Cart;
 use App\Repository\CartRepository;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class CartStorage
 {
@@ -16,6 +17,23 @@ class CartStorage
     ) {
     }
 
+    /**
+     * Get session
+     * @return null|SessionInterface 
+     */
+    private function getSession(): ?SessionInterface
+    {
+        try {
+            return $this->requestStack->getSession();
+        } catch (\Throwable $th) {
+            return null;
+        }
+    }
+
+    /**
+     * Get cart
+     * @return null|Cart 
+     */
     public function getCart(): ?Cart
     {
         $cartUuid = $this->getCartUuid();
@@ -27,13 +45,32 @@ class CartStorage
         return $this->cartRepository->findOneBy(['uuid' => $cartUuid]);
     }
 
+    /**
+     * Get cart Uuid
+     * @return null|string 
+     */
     public function getCartUuid(): ?string
     {
-        return $this->requestStack->getSession()->get(self::CART_KEY);
+        $session = $this->getSession();
+        if (!$session) {
+            return null;
+        }
+
+        return $session->get(self::CART_KEY);
     }
 
+    /**
+     * Set cart
+     * @param null|Cart $cart 
+     * @return void 
+     */
     public function setCart(?Cart $cart): void
     {
+        $session = $this->getSession();
+        if (!$session) {
+            return;
+        }
+
         if (null === $cart) {
             $this->requestStack->getSession()->remove(self::CART_KEY);
             return;
